cf47554019e33a176b2f36096e14e58f6d810bbe
Jonathanbir <jonathan79111@gmail.com>
2022-12-14 22:01:14 +0800

    commit:record last record

--- src/store/index.js
+++ src/store/index.js
@@ -11,13 +11,13 @@ export const store = createStore({
     return {
       animation: false,
       bool: { show: false, submenu: true },
-      // menudata: MENU_DATA,
-      // menu: MENU_DATA[0].data,
       menu: {
         index: 0,
         data: MENU_DATA,
         menu: MENU_DATA[0].data,
         active: [0, MENU_DATA[0].data[0].src],
+        // menu: MENU_DATA[4].data,
+        // active: [0, MENU_DATA[4].data[0].items[0].src],
       },
     };
   },
